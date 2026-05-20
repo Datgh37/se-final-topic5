fdffc38f5c56184c9b95e2a6ce08dd0e5f4fc0cb
baopham2537 <baro3275@gmail.com>
2026-05-20 09:47:29 +0700

    Xong 70%

---
+++ Controllers/CheckoutController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebUITopic5_Team4.Data;
+
+
+namespace WebUITopic5_Team4.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        private readonly ElectronicShopContext _context;
+
+        public CheckoutController(ElectronicShopContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            var accountId = User.FindFirst("AccountId")?.Value;
+
+            var cart = await _context.Carts
+                .Include(c => c.CartItems)
+                .ThenInclude(ci => ci.Product)
+                .FirstOrDefaultAsync(c => c.AccountId == accountId);
+
+            if (cart == null)
+            {
+                return RedirectToAction("Index", "Cart");
+            }
+
+            return View(cart);
+        }
+    }
+}
---
+++ wwwroot/js/checkout.js
@@ -0,0 +1,48 @@
+﻿// ===== REMOVE VIETNAMESE ACCENTS =====
+function removeVietnameseTones(str) {
+    return str
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "")
+        .replace(/đ/g, "d")
+        .replace(/Đ/g, "D")
+        .toLowerCase();
+}
+
+const input = document.getElementById("provinceInput");
+const dropdown = document.getElementById("provinceDropdown");
+const options = document.querySelectorAll(".option");
+
+// ===== OPEN DROPDOWN =====
+input.addEventListener("focus", () => {
+    dropdown.classList.add("show");
+});
+
+// ===== SEARCH FILTER =====
+input.addEventListener("input", () => {
+    const keyword = removeVietnameseTones(input.value);
+
+    options.forEach(option => {
+        const text = removeVietnameseTones(option.innerText);
+
+        if (text.includes(keyword)) {
+            option.style.display = "block";
+        } else {
+            option.style.display = "none";
+        }
+    });
+});
+
+// ===== CLICK SELECT =====
+options.forEach(option => {
+    option.addEventListener("click", () => {
+        input.value = option.innerText;
+        dropdown.classList.remove("show");
+    });
+});
+
+// ===== CLICK OUTSIDE CLOSE =====
+document.addEventListener("click", (e) => {
+    if (!e.target.closest(".custom-select")) {
+        dropdown.classList.remove("show");
+    }
+});
